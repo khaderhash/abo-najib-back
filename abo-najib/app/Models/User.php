ccc526d5c8aaed2c1ee5c6c3e43a913eff8952c9
khader huijih <164516684+khaderGg@users.noreply.github.com>
2025-02-28 17:00:45 +0300

    add  auth controller

--- abo-najib/app/Models/User.php
+++ abo-najib/app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
 use HasFactory;
     protected $fillable = [
@@ -47,5 +48,26 @@ use HasFactory;
     public function reminders()
     {
         return $this->hasMany(Reminder::class,'reminder_id');
+    }
+
+
+      /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- abo-najib/routes/api.php
+++ abo-najib/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,11 @@ Route::get('/goal/{id}',[GoalController::class,'show']);
 Route::post('/addgoal',[GoalController::class,'store']);
 Route::post('/updategoal/{id}',[GoalController::class,'update']);
 Route::post('/deletegoal/{id}',[GoalController::class,'destroy']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('profile', [AuthController::class, 'userProfile']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
